97eeb2ee6d2318b1e1b834f0779a753deecc875e
Ken Lam <kenhasbeenused@gmail.com>
2024-12-23 11:03:24 +1300

    update license with cc-nc 4.0

--- vue-web/src/components/SiteFooter.vue
+++ vue-web/src/components/SiteFooter.vue
@@ -2,8 +2,7 @@
   <hr class="border-accent" />
   <div class="flex justify-center pt-2">
     <p class="text-secondary italic">
-      <span class="capitalize">Copy all you can with reference</span>
-        @ grd0.net (2024)
+      <a href="https://creativecommons.org/licenses/by-nc/4.0/">CC BY-NC 4.0</a> @ grd0.net (2024)
     </p>
   </div>
 </template>
