30484b0fd8eab63e1dff62efba753192ccf250f8
Ken Lam <kenhasbeenused@gmail.com>
2024-12-24 11:22:03 +1300

    open license in new tab

--- vue-web/src/components/SiteFooter.vue
+++ vue-web/src/components/SiteFooter.vue
@@ -2,7 +2,7 @@
   <hr class="border-accent" />
   <div class="flex justify-center pt-2">
     <p class="text-secondary italic">
-      <a href="https://creativecommons.org/licenses/by-nc/4.0/">CC BY-NC 4.0</a> @ grd0.net (2024)
+      <a href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank">CC BY-NC 4.0</a> @ grd0.net (2024)
     </p>
   </div>
 </template>
