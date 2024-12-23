0fc68c124cffb7636724bf67500c676e958db5e2
Ken Lam <kenhasbeenused@gmail.com>
2024-12-23 21:31:03 +1300

    save metadata like exif to file

--- assets-helper/toc-generator.js
+++ assets-helper/toc-generator.js
@@ -1,9 +1,10 @@
 import fs from 'fs';
 import path from 'path';
+import ExifReader from 'exifreader';
 
 const assets_directory = '../assets';
 const output_filename = 'files.json';
-const skip_file = [import.meta.filename.split('/').reverse()[0], output_filename];
+const skip_file = ['.DS_Store'];
 
 async function getAllFilesRecursively(directory) {
   let files = [];
@@ -29,26 +30,59 @@ async function getAllFilesRecursively(directory) {
 }
 
 async function groupFilesByDirectory(files) {
-  const groups = files.map((val) => {
+  const group_actions = files.map(async (val) => {
     const path_split = val.split('/');
     const path = path_split.slice(1).reverse().slice(1);
     const filename = path_split.reverse()[0];
 
-    return {
+    const basic_info = {
       path: path ? `/${path.reverse().join('/')}` : '/',
       filename,
+    };
+
+    try {
+      const { exif } = await ExifReader.load(assets_directory + val, {
+        expanded: true,
+      });
+
+      if (exif) {
+        return {
+          ...basic_info,
+          exif: {
+            datetime: exif.DateTime?.value[0] ?? undefined,
+            shutter: exif.ExposureTime?.description ?? undefined,
+            fstop: exif.FNumber?.description ?? undefined,
+            iso: exif.ISOSpeedRatings?.description ?? undefined,
+            focal: exif.FocalLengthIn35mmFilm?.description ?? undefined,
+            equipment: {
+              camera: exif.Make || exif.Model ? `${exif.Make?.description} ${exif.Model?.description}` : undefined,
+              lens: exif.LensModel?.description ?? undefined,
+            },
+          },
+        };
+      }
+    } catch (err) {
+      console.error(err);
     }
+
+    return basic_info;
+
   });
+
+  const groups = await Promise.all(group_actions);
 
   const group_by = Object.groupBy(groups, ({ path }) => path);
 
-  return flattenGroups(group_by);
+  return formatGroups(group_by);
 }
 
-async function flattenGroups(groups) {
+async function formatGroups(groups) {
   for (let key in groups) {
-    groups[key] = groups[key].map((val) => {
-      return val.filename;
+    groups[key] = groups[key].map(({filename, exif}) => {
+      return {
+        filename,
+        exif,
+      };
     });
   }
 
@@ -60,7 +94,7 @@ async function main() {
 
   let grouped = await groupFilesByDirectory(allFiles);
 
-  fs.writeFile(output_filename, JSON.stringify(grouped), 'utf8', (err) => {
+  fs.writeFile(assets_directory + '/' + output_filename, JSON.stringify(grouped), 'utf8', (err) => {
     if (err) {
       console.log("Error writing file");
       return;
