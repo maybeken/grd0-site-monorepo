93b21770e98dd074f88d6ab5651cf4bdee24dd18
Ken Lam <kenhasbeenused@gmail.com>
2025-01-17 10:10:07 +1300

    feature: change skeleton to fully rounded

--- vue-web/src/components/Skeleton.vue
+++ vue-web/src/components/Skeleton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="loading" class="rounded-xl overflow-hidden bg-gradient-to-r from-accent via-secondary to-accent blur-sm" :class="`h-${h} w-${w}`">
+  <div v-if="loading" class="rounded-full overflow-hidden bg-gradient-to-r from-accent via-secondary to-accent blur-sm" :class="`h-${h} w-${w}`">
     <div class="bg-gradient-to-r from-secondary to-accent rounded-full h-full w-full motion-translate-x-loop-100 motion-duration-1000 motion-ease-spring-smooth"></div>
   </div>
   <slot v-else />
