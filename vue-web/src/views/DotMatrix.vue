c14b9fd1dc90b223eefc053e3e0296759c64d59e
Ken Lam <ken.lam@grd0.net>
2025-01-22 19:22:36 +1300

    feature: allow changing gap of generated dot matrix

--- vue-web/src/views/DotMatrix.vue
+++ vue-web/src/views/DotMatrix.vue
@@ -3,15 +3,17 @@
     <div class="flex gap-2">
       <label>Finest:</label>
       <input class="bg-inherit appearance-none text-center" type="number" v-model="finest" />
+      <label>Gap:</label>
+      <input class="bg-inherit appearance-none text-center" type="number" v-model="gap" />
     </div>
     <div>
       <button class="px-4 py-px border rounded-lg" @click="getSvg">Copy</button>
     </div>
   </div>
   <div class="flex mx-auto bg-white w-64">
-    <svg id="svg" xmlns="http://www.w3.org/2000/svg" :viewBox="`${-(finest * 3 * size + size)} ${-(finest * 3 * size + size)} ${(finest * 3 * size + size) * 2} ${(finest * 3 * size + size) * 2}`" fill="black">
+    <svg id="svg" xmlns="http://www.w3.org/2000/svg" :viewBox="`${-(finest * size + gap * size * 2)} ${-(finest * size + gap * size * 2)} ${(finest * size + gap * size * 2) * 2} ${(finest * size + gap * size * 2) * 2}`" fill="black">
       <g v-for="x in Array.from({ length: 2 * finest + 1 }, (_, i) => i - finest)" :key="x">
-        <circle v-for="y in Array.from({ length: 2 * finest + 1 }, (_, i) => i - finest)" :cx="x * size * 3" :cy="y * size * 3" :r="size" :key="y"/>
+        <circle v-for="y in Array.from({ length: 2 * finest + 1 }, (_, i) => i - finest)" :cx="x * (size * 2 + gap)" :cy="y * (size * 2 + gap)" :r="size" :key="y"/>
       </g>
     </svg>
   </div>
@@ -21,6 +23,7 @@
 import { ref } from 'vue';
 
 const size = ref(8);
+const gap = ref(8);
 const finest = ref(1);
 
 function getSvg(){
