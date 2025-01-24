dded162c454524ac3193b04486fe14b09640210c
Ken Lam <ken.lam@grd0.net>
2025-01-25 11:15:12 +1300

    devops: update license text

--- vue-web/src/components/SiteFooter.vue
+++ vue-web/src/components/SiteFooter.vue
@@ -2,11 +2,13 @@
   <hr class="border-accent" />
   <div class="flex justify-center pt-2">
     <div class="text-center">
-      <p class="text-secondary italic">
-      <a href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank">CC BY-NC 4.0</a> @ grd0.net
+      <p class="text-secondary italic text-sm">
+        Est. 2020 - {{ dayjs().format("YYYY") }} @ <a href="https://grd0.net">grd0.net</a>
       </p>
       <p class="text-secondary text-xs">
-        Est. 2020 - {{ dayjs().format("YYYY") }} | <a href="http://gitlab.com/grd0.net/grd0-site-monorepo" target="_blank">Source Available @ GitLab</a>
+        <a href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank">Site content licensed under CC BY-NC 4.0</a>
+        <span class="select-none"> | </span>
+        <a href="http://gitlab.com/grd0.net/grd0-site-monorepo" target="_blank">Source code available under CC BY-SA 4.0 on GitLab</a>
       </p>
     </div>
   </div>
