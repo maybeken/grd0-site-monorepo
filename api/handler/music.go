d308db299749b1d36a949b270c4fdf27b9104146
Ken Lam <ken.lam@grd0.net>
2025-03-31 19:12:01 +1300

    refactor: soft delete logic

--- api/handler/music.go
+++ api/handler/music.go
@@ -29,7 +29,7 @@ func (h *Handler) UpsertMusic(c echo.Context) error {
 	}
 
 	if err := db.Create(&music).Error; errors.Is(err, gorm.ErrDuplicatedKey) {
-		if err := db.Model(schema.Music{}).Where("v = ?", music.V).Updates(&music).Error; err != nil {
+		if err := db.Unscoped().Model(schema.Music{}).Where("v = ?", music.V).Updates(&music).Update("deleted_at", nil).Error; err != nil {
 			return h.ErrorResponseConstructor(c, http.StatusInternalServerError, err.Error())
 		}
 	} else if err != nil {
@@ -44,7 +44,7 @@ func (h *Handler) DeleteMusic(c echo.Context) error {
 
 	db := h.DB
 
-	res := db.Where("v = ?", vid).Unscoped().Delete(&schema.Music{})
+	res := db.Where("v = ?", vid).Delete(&schema.Music{})
 	row_count := res.RowsAffected
 	err := res.Error
 
