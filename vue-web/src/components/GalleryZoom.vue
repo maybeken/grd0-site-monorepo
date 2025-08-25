35a2ea3cb9c2621ae91f61745d40651b90d1252d
maybeken <ken.lam@grd0.net>
2025-08-25 20:12:36 +1200

    fix: zoom view issue with vertical images

--- vue-web/src/components/GalleryZoom.vue
+++ vue-web/src/components/GalleryZoom.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="hidden lg:block fixed top-0 left-0 w-screen h-lvh z-50 bg-shade bg-opacity-50 backdrop-blur-xs motion-preset-fade-lg">
     <div class="flex w-full h-full" @click="$emit('close')">
-      <div class="w-2/3 h-fit mx-auto my-auto p-4 rounded-2xl bg-shade text-secondary" @click="(event) => event.stopImmediatePropagation()">
+      <div class="w-fit h-fit mx-auto my-auto p-4 rounded-2xl bg-shade text-secondary" @click="(event) => event.stopImmediatePropagation()">
         <div class="flex flex-row-reverse gap-4">
           <button @click="$emit('close')"><Icon icon="mynaui:x-circle" height="auto"></Icon></button>
         </div>
-        <div class="max-h-lvh overflow-scroll">
-          <img class="w-full rounded-xl" :src="`${ASSET_URL}${uri}`" />
+        <div class="h-screen">
+          <img class="mx-auto h-[90%] object-scale-down rounded-xl" :src="`${ASSET_URL}${uri}`" />
         </div>
       </div>
     </div>
